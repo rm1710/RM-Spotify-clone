b3420a10c6235ab78eb51d48d8b53e39743d9632
rm1710 <d290mishraritesh@gmail.com>
2024-07-23 18:18:43 +0530

    done

--- rm-spotify-clone/src/context/PlayerContext.jsx
+++ rm-spotify-clone/src/context/PlayerContext.jsx
@@ -32,9 +32,35 @@ const PlayerContextProvider = (props) => {
         setPlayStatus(false)
     }
 
+    const playWithId = async (id) => {
+        await setTrack(songsData[id]);
+        await audioRef.current.play();
+        setPlayStatus(true);
+    }
+
+    const previous = async () =>{
+        if(track.id>0){
+            await setTrack(songsData[track.id-1]);
+            await audioRef.current.play();
+            setPlayStatus(true);
+        }
+    }
+    const next = async () =>{
+        if(track.id<songsData.length-1){
+            await setTrack(songsData[track.id+1]);
+            await audioRef.current.play();
+            setPlayStatus(true);
+        }
+    }
+
+    const seekSong = async (e) => {
+        audioRef.current.currentTime = ((e.nativeEvent.offsetX / seekBg.current.offsetWidth) * audioRef.current.duration);
+    }
+
     useEffect(() => {
         setTimeout(()=>{
             audioRef.current.ontimeupdate=()=>{
+                seekBar.current.style.width = (Math.floor(audioRef.current.currentTime/audioRef.current.duration*100))+"%"
                 setTime({
                     currentTime:{
                         second:Math.floor(audioRef.current.currentTime % 60),
@@ -56,7 +82,12 @@ const PlayerContextProvider = (props) => {
         track,setTrack,
         playStatus,setPlayStatus,
         time,setTime,
-        play,pause
+        play,pause,
+        playWithId,
+        previous,
+        next,
+        seekSong
+
     }
     return (
         <PlayerContext.Provider value={contextValue}>
